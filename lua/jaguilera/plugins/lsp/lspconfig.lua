b6b99209863aeb9646e4503b151afe3163b79b10
Jesus Aguilera <jaguilera@MacBook-Pro-de-Jesus.local>
2023-12-17 11:27:01 +0100

    remove ^M from files copied

--- lua/jaguilera/plugins/lsp/lspconfig.lua
+++ lua/jaguilera/plugins/lsp/lspconfig.lua
@@ -24,10 +24,6 @@ vim.o.updatetime = 250
 vim.cmd([[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
 
--- Remove ^M characters from files
-vim.cmd([[autocmd BufReadPost * %s/\r//g]])
-
-
 -- Diagnostic symbols in the sign column {gutter}
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
